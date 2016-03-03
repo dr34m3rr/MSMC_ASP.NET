b98717f053f955724ea7e79def13e44737b9f70f
dR34m3r <shout.exe@yandex.ru>
2016-03-04 01:33:01 +0300

    timus1025,800kb/0.031

---
+++ Csh/1025/timus1025/timus1025/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace timus1025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double answer = 0;
+            int n = int.Parse(Console.ReadLine());
+            string[] temp = new string[n];
+            int[] arr = new int[n];
+            temp = Console.ReadLine().Split(' ');
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(temp[i]);
+            }
+            Array.Sort(arr);
+            double groups = Math.Ceiling(arr.Length / 2.0);
+            for (int i = 0; i < groups; ++i)
+            {
+                answer += (Math.Ceiling(arr[i] / 2.0));
+            }
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+
+    }
+}
