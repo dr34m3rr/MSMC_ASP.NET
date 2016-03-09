5f9bd18eafaf9891b4a747ec979217ac2b99e451
dR34m3r <shout.exe@yandex.ru>
2016-03-09 05:27:30 +0300

    banditos, accepted

---
+++ Csh/1409/1409/1409/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1409
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] temp;
+            int[] hit, didnthit;
+            hit = new int[2];
+            didnthit = new int[2];
+            temp = Console.ReadLine().Split(' ');
+            for(int i=0;i<2;i++)
+            {
+                hit[i] = int.Parse(temp[i]);
+            }
+            didnthit[1] = hit[0] - 1;
+            didnthit[0] = hit[1] - 1;
+            Console.Write("{0} {1}", didnthit[0], didnthit[1]);
+        }
+    }
+}
