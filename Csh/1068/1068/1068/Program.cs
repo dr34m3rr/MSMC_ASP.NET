13fb2c5f8afa2073381b0243c272b4326b9a76cf
dR34m3r <shout.exe@yandex.ru>
2016-03-09 05:56:18 +0300

    summ

---
+++ Csh/1068/1068/1068/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1068
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, result = 0;
+            N = int.Parse(Console.ReadLine());
+            while (N != 1)
+                {
+                result += N;
+                if (N < 0)
+                    N++;
+                else
+                    if (N == 0)
+                    N++;
+                else
+                    N--;
+            }
+            Console.WriteLine(result + 1);
+        }
+    }
+}
